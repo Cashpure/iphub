af7a9c1c5283ce5ab0ecafef9a212c7c01c7328f
MatveyKa <govrinisranbik@gmail.com>
2025-01-30 21:57:10 +0300

    complex

--- script/script.js
+++ script/script.js
@@ -69,6 +69,11 @@ $(function () {
       e.preventDefault();
       return false;
    });
+
+   $('.faq-complex__acc-item').on('click', function () {
+      $(this).toggleClass('faq-complex__acc-item--active')
+      $(this).children('.acc-text').slideToggle()
+   })
 })
 
 let button = document.getElementsByClassName('button')
@@ -252,6 +257,126 @@ const swiper4 = new Swiper('.swiper-4', {
    navigation: {
       nextEl: '.slider__arrow-right-4',
       prevEl: '.slider__arrow-left-4',
+   },
+});
+const swiper5 = new Swiper('.swiper-5', {
+   // Optional parameters
+   direction: 'horizontal',
+   loop: true,
+   spaceBetween: 20,
+   allowTouchMove: true,
+   slidesPerView: 'auto',
+   // centeredSlides: true,
+   // breakpoints: {
+   //    0: {
+   //       slidesPerView: 1,
+   //    },
+   //    580: {
+   //       slidesPerView: 2,
+   //    },
+   //    868: {
+   //       slidesPerView: 3,
+   //    },
+   //    1100: {
+   //       slidesPerView: 4,
+   //    },
+   // },
+});
+const swiper6 = new Swiper('.swiper-6', {
+   // Optional parameters
+   direction: 'horizontal',
+   loop: false,
+   spaceBetween: 34,
+   allowTouchMove: false,
+   slidesPerView: '5',
+   // centeredSlides: true,
+   breakpoints: {
+      0: {
+         slidesPerView: 2,
+      },
+      680: {
+         slidesPerView: 3,
+      },
+      1000: {
+         slidesPerView: 4,
+      },
+      1200: {
+         slidesPerView: 5,
+      },
+   },
+   
+   
+   // Navigation arrows
+   navigation: {
+      nextEl: '.slider__arrow-right-6',
+      prevEl: '.slider__arrow-left-6',
+   },
+});
+
+
+let slide6 = document.querySelectorAll('.slide-6')
+let arrowRight6 = document.querySelectorAll('.slider__arrow-right-6')
+let arrowLeft6 = document.querySelectorAll('.slider__arrow-left-6')
+
+
+let s = 0
+function slider6Right () {
+   if (s <= 3) {
+      s++
+      slide6[s].classList.add('process-slide--active')
+   }
+   console.log(s)
+   console.log(slider6Right)
+   console.log(slide6)
+}
+function slider6Left () {
+   if (s >= 1) {
+      slide6[s].classList.remove('process-slide--active')
+      s--
+   }
+   console.log(s)
+   console.log(slider6Right)
+   console.log(slide6)
+}
+
+
+
+console.log(arrowRight6)
+arrowRight6[0].onclick = slider6Right
+arrowLeft6[0].onclick = slider6Left
+arrowRight6[1].onclick = slider6Right
+arrowLeft6[1].onclick = slider6Left
+
+
+
+const swiper7 = new Swiper('.swiper-7', {
+   // Optional parameters
+   direction: 'horizontal',
+   loop: false,
+   spaceBetween: 340,
+   allowTouchMove: false,
+   slidesPerView: '1',
+   // centeredSlides: true,
+   // breakpoints: {
+   //    0: {
+   //       slidesPerView: 1,
+   //    },
+   //    580: {
+   //       slidesPerView: 2,
+   //    },
+   //    868: {
+   //       slidesPerView: 3,
+   //    },
+   //    1100: {
+   //       slidesPerView: 4,
+   //    },
+   // },
+   
+   
+   // Navigation arrows
+   navigation: {
+      nextEl: '.slider__arrow-right-6',
+      prevEl: '.slider__arrow-left-6',
    },
 });
 
