75e30725734f5f2799dc7c654cfec3b78bf2658c
MatveyKa <govrinisranbik@gmail.com>
2025-02-14 08:20:33 +0300

    -unusable swiper

--- script/script-cases.js
+++ script/script-cases.js
@@ -2,6 +2,6 @@ let casesItem = document.querySelectorAll('.header-cases__grid-item')
 
 Array.from(casesItem).forEach(element => {
    element.onclick = () => {
-      window.open('case.html')
+      window.open('case.html', '_self')
    } 
 })
