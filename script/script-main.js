10cf0b62b2061890e6d526c06b951d4761a0f4eb
MatveyKa <govrinisranbik@gmail.com>
2025-02-14 08:33:17 +0300

    fixes

--- script/script-main.js
+++ script/script-main.js
@@ -14,6 +14,139 @@ Array.from(swiperSlide2).forEach(element => {
    }
 })
 
+let prev = document.getElementById('prev')
+let next = document.getElementById('next')
+let counter = document.getElementById('qustion__counter')
+let question = document.getElementById('question')
+let answer1 = document.querySelector('.answer-1')
+let answer2 = document.querySelector('.answer-2')
+let answer3 = document.querySelector('.answer-3')
+let answer4 = document.querySelector('.answer-4')
+let answer5 = document.querySelector('.answer-5')
+let answer6 = document.querySelector('.answer-6')
+let n = 1
+
+const pages = [
+   {
+      questionText: "С какой целью вы обращаетесь?",
+      "answers": ["Регистрацию полезной модели", "Регистрацию прав на использование полезной модели, промобразца, изобретенния", "Регистрацию изобретения", "Регистрацию промышленного образца", "Подготовка заявки на выдачу патента", "Подготовка заявки на передачу прав патента"]
+   },
+   {
+      questionText: "2-й вопрос",
+      "answers": ["Регистрацию полезной модели", "Регистрацию прав на использование полезной модели, промобразца, изобретенния", "Регистрацию изобретения", "Регистрацию промышленного образца", "Подготовка заявки на выдачу патента", "Подготовка заявки на передачу прав патента"]
+   },
+   {
+      questionText: "3-й вопрос",
+      "answers": ["Регистрацию полезной модели", "Регистрацию прав на использование полезной модели, промобразца, изобретенния", "Регистрацию изобретения", "Регистрацию промышленного образца", "Подготовка заявки на выдачу патента", "Подготовка заявки на передачу прав патента"]
+   },
+   {
+      questionText: "4-й вопрос",
+      "answers": ["Регистрацию полезной модели", "Регистрацию прав на использование полезной модели, промобразца, изобретенния", "Регистрацию изобретения", "Регистрацию промышленного образца", "Подготовка заявки на выдачу патента", "Подготовка заявки на передачу прав патента"]
+   },
+   {
+      questionText: "5-й вопрос",
+      "answers": ["Регистрацию полезной модели", "Регистрацию прав на использование полезной модели, промобразца, изобретенния", "Регистрацию изобретения", "Регистрацию промышленного образца", "Подготовка заявки на выдачу патента", "Подготовка заявки на передачу прав патента"]
+   },
+   {
+      questionText: "6-й вопрос",
+      "answers": ["Регистрацию полезной модели", "Регистрацию прав на использование полезной модели, промобразца, изобретенния", "Регистрацию изобретения", "Регистрацию промышленного образца", "Подготовка заявки на выдачу патента", "Подготовка заявки на передачу прав патента"]
+   },
+   {
+      questionText: "7-й вопрос",
+      "answers": ["Регистрацию полезной модели", "Регистрацию прав на использование полезной модели, промобразца, изобретенния", "Регистрацию изобретения", "Регистрацию промышленного образца", "Подготовка заявки на выдачу патента", "Подготовка заявки на передачу прав патента"]
+   },
+   {
+      questionText: "8-й вопрос",
+      "answers": ["Регистрацию полезной модели", "Регистрацию прав на использование полезной модели, промобразца, изобретенния", "Регистрацию изобретения", "Регистрацию промышленного образца", "Подготовка заявки на выдачу патента", "Подготовка заявки на передачу прав патента"]
+   },
+]
+
+const swiper = new Swiper('.swiper-1', {
+   // Optional parameters
+   direction: 'horizontal',
+   loop: false,
+   spaceBetween: 20,
+   allowTouchMove: true,
+   slidesPerView: 'auto',
+   // centeredSlides: true,
+   breakpoints: {
+      0: {
+         slidesPerView: 1,
+      },
+      580: {
+         slidesPerView: 2,
+      },
+      868: {
+         slidesPerView: 3,
+      },
+      1100: {
+         slidesPerView: 4,
+      },
+   },
+
+
+   // Navigation arrows
+   navigation: {
+      nextEl: '.slider__arrow-right',
+      prevEl: '.slider__arrow-left',
+   },
+});
+
+const swiper2 = new Swiper('.swiper-2', {
+   // Optional parameters
+   direction: 'horizontal',
+   loop: false,
+   spaceBetween: 20,
+   allowTouchMove: true,
+   slidesPerView: 'auto',
+   // centeredSlides: true,
+   breakpoints: {
+      0: {
+         slidesPerView: 1,
+      },
+
+      1100: {
+         slidesPerView: 2,
+      },
+   },
+
+
+   // Navigation arrows
+   navigation: {
+      nextEl: '.slider__arrow-right-2',
+      prevEl: '.slider__arrow-left-2',
+   },
+});
+
+const swiper3 = new Swiper('.swiper-3', {
+   // Optional parameters
+   direction: 'horizontal',
+   loop: false,
+   spaceBetween: 20,
+   allowTouchMove: true,
+   slidesPerView: 'auto',
+   // centeredSlides: true,
+   breakpoints: {
+      0: {
+         slidesPerView: 1,
+      },
+      480: {
+         slidesPerView: 2,
+      },
+
+      1100: {
+         slidesPerView: 4,
+      },
+   },
+
+
+   // Navigation arrows
+   navigation: {
+      nextEl: '.slider__arrow-right-3',
+      prevEl: '.slider__arrow-left-3',
+   },
+});
+
 
 
 function update(page) {
--- script/script-service.js
+++ script/script-service.js
@@ -5,3 +5,34 @@ Array.from(swiperSlide4).forEach(el => {
       window.open('service.html', '_self')
    }
 })
+
+const swiper4 = new Swiper('.swiper-4', {
+   // Optional parameters
+   direction: 'horizontal',
+   loop: false,
+   spaceBetween: 20,
+   allowTouchMove: true,
+   slidesPerView: 'auto',
+   // centeredSlides: true,
+   breakpoints: {
+      0: {
+         slidesPerView: 1,
+      },
+      580: {
+         slidesPerView: 2,
+      },
+      868: {
+         slidesPerView: 3,
+      },
+      1100: {
+         slidesPerView: 4,
+      },
+   },
+   
+   
+   // Navigation arrows
+   navigation: {
+      nextEl: '.slider__arrow-right-4',
+      prevEl: '.slider__arrow-left-4',
+   },
+});
